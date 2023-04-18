3b427d4e57ff0db240b0b9f23e25e6d7cbac142d
Oliver Weber <ow@uix.io>
2023-04-18 19:07:42 +0200

    add(backend): 02_Node Lev2_1 snippet

---
+++ 12_backend/02_Node.js/Lev2_1_import-export-filter/data.js
@@ -0,0 +1,81 @@
+const data = [{
+  "id": 1,
+  "city": "Asempapak",
+  "population": 955009
+}, {
+  "id": 2,
+  "city": "Póvoa",
+  "population": 746140
+}, {
+  "id": 3,
+  "city": "Aniva",
+  "population": 214032
+}, {
+  "id": 4,
+  "city": "San Narciso",
+  "population": 207491
+}, {
+  "id": 5,
+  "city": "Edissiya",
+  "population": 919145
+}, {
+  "id": 6,
+  "city": "Sukabatu",
+  "population": 427949
+}, {
+  "id": 7,
+  "city": "Kut Chap",
+  "population": 785071
+}, {
+  "id": 8,
+  "city": "Västerås",
+  "population": 347768
+}, {
+  "id": 9,
+  "city": "Daluo",
+  "population": 649013
+}, {
+  "id": 10,
+  "city": "Suwaduk",
+  "population": 409331
+}, {
+  "id": 11,
+  "city": "Basseterre",
+  "population": 71597
+}, {
+  "id": 12,
+  "city": "Mayfa‘ah",
+  "population": 227212
+}, {
+  "id": 13,
+  "city": "Panitian",
+  "population": 533906
+}, {
+  "id": 14,
+  "city": "Stockholm",
+  "population": 589823
+}, {
+  "id": 15,
+  "city": "Póvoa",
+  "population": 611489
+}, {
+  "id": 16,
+  "city": "Irvine",
+  "population": 78890
+}, {
+  "id": 17,
+  "city": "Liqi",
+  "population": 926611
+}, {
+  "id": 18,
+  "city": "Solotcha",
+  "population": 617802
+}, {
+  "id": 19,
+  "city": "Červené Pečky",
+  "population": 753278
+}, {
+  "id": 20,
+  "city": "Çepan",
+  "population": 787308
+}]
