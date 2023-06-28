3c0432a6cb678f2b1817255fa1bd30cf48884cc2
Lea Heidjann <leaheidjann@gmail.com>
2023-06-28 10:54:32 +0200

    snippet for js_vertiefung_intro_level_1_13

---
+++ 09_js_vertiefung/js_vertiefung_intro/js_vertiefung_intro_level_1_13/main.js
@@ -0,0 +1,15 @@
+// Addition
+let addition_operator = 1 + 1;
+console.log("addition: " + addition_operator);
+ // Subtraktion
+let subtraktion_operator = 2 - 1;
+console.log("subtraktion: " + subtraktion_operator);
+// Multiplikation
+let multiplication_operator = 2 * 2;
+console.log("multiplication: " + multiplication_operator);
+// Division
+let division_operator = 4 / 2;
+console.log("division: " + division_operator);
+// Modul: zeigt an, was der Rest sein würde.
+let modulus_operator = 14 % 5;
+console.log("modulus: " + modulus_operator);
