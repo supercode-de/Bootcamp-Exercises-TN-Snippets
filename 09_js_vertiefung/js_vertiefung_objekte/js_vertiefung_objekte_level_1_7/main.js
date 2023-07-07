939d37cf73c8c5ad6d5c39604eb4d92240251d45
Steffi <peschkestefanie@web.de>
2023-07-07 14:03:38 +0200

    JS-Vertiefung-Objekte-Level-1_7

---
+++ 09_js_vertiefung/js_vertiefung_objekte/js_vertiefung_objekte_level_1_7/main.js
@@ -0,0 +1,30 @@
+let myMusic = [
+    {
+        artist: "The Beatles",
+        title: "Abbey Road",
+        release_year: 1969,
+        medium: ["LP", "CD", "MC", "Download"],
+        gold: true
+    },
+    {
+        artist: "Pink Floyd",
+        title: "Dark Side of the Moon",
+        release_year: 1978,
+        medium: ["CS", "CD", "LP", "Download"],
+        gold: true
+    },
+    {
+        artist: "Led Zeppelin",
+        title: "Led Zeppelin IV",
+        release_year: 1971,
+        medium: ["CS", "LP", "Download"],
+        gold: true
+    },
+    {
+        artist: "Metallica",
+        title: "Kill ’Em All und Ride the Lightning",
+        release_year: 1983,
+        medium: ["LP", "CD", "MC", "Download"],
+        gold: true
+    }
+];
