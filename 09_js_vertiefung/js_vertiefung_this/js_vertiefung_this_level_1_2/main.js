77797127a4f5bcbe57846aa5fa953081e5f6fd01
Steffi <peschkestefanie@web.de>
2023-07-10 15:10:07 +0200

    added JS-Vertiefung-This-Level-1_2

---
+++ 09_js_vertiefung/js_vertiefung_this/js_vertiefung_this_level_1_2/main.js
@@ -0,0 +1,10 @@
+// let person = {
+//     vorname: 'Anton',
+//     nachname: 'Nüssler',
+//     alter: 34,
+//     familienstand: 'ledig',
+//     groesse: '178cm',
+//     profil: function () {
+//         return this...;
+// }
+// };
