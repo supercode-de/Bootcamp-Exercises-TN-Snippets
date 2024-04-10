1fc490d483cd9759555549633590b42a1a90056d
Steffi <peschkestefanie@web.de>
2024-04-10 08:07:02 -0300

    created intro typescript aufgabe

--- 06_ts_grundlagen/ts_einfuehrung_intro/ts_vertiefung_intro_level_1_13/main.ts
+++ 06_ts_grundlagen/ts_einfuehrung_intro/ts_vertiefung_intro_level_1_13/main.ts
@@ -1,15 +1,19 @@
 // Addition
-let addition_operator = 1 + 1;
+let addition_operator: number = 1 + 1;
 console.log("addition: " + addition_operator);
- // Subtraktion
-let subtraktion_operator = 2 - 1;
+
+// Subtraktion
+let subtraktion_operator: number = 2 - 1;
 console.log("subtraktion: " + subtraktion_operator);
+
 // Multiplikation
-let multiplication_operator = 2 * 2;
+let multiplication_operator: number = 2 * 2;
 console.log("multiplication: " + multiplication_operator);
+
 // Division
-let division_operator = 4 / 2;
+let division_operator: number = 4 / 2;
 console.log("division: " + division_operator);
+
 // Modul: zeigt an, was der Rest sein würde.
-let modulus_operator = 14 % 5;
+let modulus_operator: number = 14 % 5;
 console.log("modulus: " + modulus_operator);
