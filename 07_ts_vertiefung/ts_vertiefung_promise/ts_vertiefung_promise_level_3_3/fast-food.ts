91ab31369cf1f40d1e0bf2f6eab3b8a48be94db8
Alexandra Wittmann <alex@super-code.de>
2024-06-13 15:50:30 +0200

    feat: add ts vertiefung promises level 3.3

---
+++ 07_ts_vertiefung/ts_vertiefung_promise/ts_vertiefung_promise_level_3_3/fast-food.ts
@@ -0,0 +1,29 @@
+document.getElementById('startSimulation')!.addEventListener('click', () => {
+  const ordersDiv = document.getElementById('orders')!;
+  ordersDiv.innerHTML = '';
+
+  // createCustomerOrder(1, 'Cola');
+  // createCustomerOrder(2, 'Sprite');
+  // createCustomerOrder(3, 'Wasser');
+});
+
+//  hier die Function einfügen, die Promises zurückgeben
+
+function processOrder(customerNumber: number, drink: string, customerDiv: HTMLDivElement) {
+  // Promise.all([
+  //   order(customerNumber, customerDiv),
+  //   pay(customerDiv),
+  //   makeBurger(customerDiv),
+  //   makeFries(customerDiv),
+  //   makeDrink(drink, customerDiv)
+  // ]).then(() => {
+  //   console.log('Bestellung abgeschlossen.');
+  //   customerDiv.innerHTML += '✅ Bestellung abgeschlossen.<br>';
+  // });
+}
+
+function getRandomDurationInMilliseconds(): number {
+  const min = 2000;
+  const max = 12000;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
